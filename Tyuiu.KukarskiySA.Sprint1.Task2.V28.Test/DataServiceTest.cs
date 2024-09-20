a828141e1671f78e306d38acd3fede7cc6a5c014
KukarskiySergey <Tyuiu.KukarskiySergey@outlook.com>
2024-09-21 03:17:53 +0500

    Repair Task2

--- Tyuiu.KukarskiySA.Sprint1.Task2.V28.Test/DataServiceTest.cs
+++ Tyuiu.KukarskiySA.Sprint1.Task2.V28.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.KukarskiySA.Sprint1.Task2.V28.Test
             DataService dataService = new DataService();
             int celsius = 0;
             int kelvin = 273;
-            var result = dataService.Calculate(celsius + 273);
+            var result = dataService.Calculate(celsius);
 
             Assert.AreEqual(273, result);
         }
